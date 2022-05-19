2c2615e58fef3173f72abb333955bb95ffc86943
tabarksaleh <tabarksaleh1997@gmail.com>
2022-05-20 00:52:41 +0200

    1- Add Second Level & Third Level

--- src/Levels/SecondLevelModel.java
+++ src/Levels/SecondLevelModel.java
@@ -3,12 +3,59 @@ import MainPackage.*;
 
 public class SecondLevelModel extends GameModel {
     public SecondLevelModel(){
-        super(22, 11);
+        super(8, 9);
+        setPlayerPos(2, 1);
 
-        setPlayerPos(20, 9);
+        addCrateBox(3, 2);
+        addCrateBox(2, 5);
+        addCrateBox(5, 6);
+
+
+        addWall(2, 3);
+        addWall(2, 4);
+        addWall(4, 3);
+        addWall(4, 4);
+        addWall(4, 1);
+        addWall(5, 5);
+
+
+
+        for (int i = 0; i < 6; i++) {
+            addWall(1+i, 0);
+
+        }
+        for (int i = 0; i < 3; i++) {
+            addWall(6, 1+i);
+
+        }
+
+        for (int i = 0; i < 4; i++) {
+            addWall(7, 4+i);
+        }
+        for (int i = 0; i < 7; i++) {
+            addWall(0, 1+i);
+        }
+        for (int i = 0; i < 6; i++) {
+            addWall(1+i, 7);
+        }
+
+
+        for (int i = 0; i < 3; i++) {
+            addWall(1, 1+i);
+        }
+
+        for (int i = 0; i < 4; i++) {
+            addMarkedBox(1, 3+i);
+        }
+
+
+
+      /*  super(22, 11);
+
+        setPlayerPos(3, 3);
+
+
         addCrateBox(21, 9);
-
-        /*
         addCrateBox(4, 3);
         addCrateBox(4, 4);
         addCrateBox(1, 6);
@@ -24,27 +71,29 @@ public class SecondLevelModel extends GameModel {
         addMarkedBox(6, 6);
         addMarkedBox(4, 7);
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 19; i++) {
             addWall(2+i, 0);
-            addWall(6, 1+i);
+            //addWall(5, 1+i);
         }
-        for (int i = 0; i < 4; i++) {
-            addWall(7, 5+i);
-        }
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < 10; i++) {
             addWall(0, 1+i);
         }
-        for (int i = 0; i < 6; i++) {
-            addWall(1+i, 8);
+        for (int i = 0; i < 20; i++) {
+            addWall(1+i, 10);
         }
-        addWall(1, 1);
+        for (int i = 0; i < 6; i++) {
+            addWall(21, 5+i);
+        }
+
+
+        addWall(10, 0);
         addWall(2, 1);
         addWall(1, 3);
         addWall(2, 3);
         addWall(2, 4);
         addWall(3, 4);
         addWall(2, 5);
+*/
 
-         */
     }
 }
